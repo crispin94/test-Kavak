d019d64ee5675807f9aa2f69b5a668cb6e380cf5
CRISPIN <abraham.crispin@gnp.com.mx>
2019-11-11 21:58:23 -0600

    delete css selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Service } from '../app/servicios/service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'AngularTest';
